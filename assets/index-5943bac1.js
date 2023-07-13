de9bca941b558d14206f6ae4c40c9954f1e20ddf
annavilnid <anna.vilnid@gmail.com>
2023-07-13 13:49:38 +0200

    Updates

--- assets/index-5943bac1.js
+++ assets/index-5943bac1.js
@@ -129,12 +129,12 @@ Error generating stack: `+o.message+`
   width: 100%;
   height: auto;
   min-height: 63.47vw;
-  background-image: url("/src/images/main-screen/screen-L.svg");
+  background-image: url("/thoth/images/main-screen/screen-L.svg");
 
   @media (max-width: ${$}) {
     width: 100vw;
     min-height: 209.25vw;
-    background-image: url("/src/images/main-screen/screen-S.svg");
+    background-image: url("/thoth/images/main-screen/screen-L.svg");
   }
 `,Qw=S.div`
   max-width: 50.76vw;
